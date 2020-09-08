aa3f4b4fa0cb78e71f8ddb99b5b4b06a1f787648
Rizart Dokollari <r.dokollari@gmail.com>
2020-09-08 19:35:14 +0100

    Install NextJS.

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Expo NextJS Template</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Welcome to Expo + Next.js 👋 v3</Text>
     </View>
   );
 }
@@ -14,8 +12,10 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 16,
   },
 });
